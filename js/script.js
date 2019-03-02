e23d43039014488977ba83b8c2fa642399fe54f6
Blake Burroughs <b.burro@gmail.com>
2019-03-02 14:29:37 -0600

    Finished and refactored

--- js/script.js
+++ js/script.js
@@ -227,21 +227,6 @@ $('#cvv').on('input', function(e) {
   }
 });
 
-//FX validate to refactor form validation onSubmit
-function validate(e, check, errorMessage) {
-
-  if (check === false) {
-    $('#error').text(errorMessage);
-
-    return e.preventDefault();
-  }
-  else {
-    $('#error').text('');
-    $(this).unbind('submit').submit();
-  }
-
-}
-
 //click handler on submit button tht validates form before submitting
 $("button[type='submit']").on('click', function(e) {
     //check credit card validation
@@ -271,11 +256,10 @@ $("button[type='submit']").on('click', function(e) {
       e.preventDefault();
     }
     //check name field and handle error
-    validate(e, $nameIsValid, 'Please enter a first and last name');
-    // if($nameIsValid === false){
-    //   $('#error').text('Please enter a first and last name.');
-    //   e.preventDefault();
-    // }
+    if($nameIsValid === false){
+       $('#error').text('Please enter a first and last name.');
+       e.preventDefault();
+    }
 
 
 })
